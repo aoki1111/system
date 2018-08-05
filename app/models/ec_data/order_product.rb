72235ff1b84184b67bcb634812a7c382da8da70f
Y.Suzuki <ys3128@gmail.com>
2018-08-05 16:45:10 +0900

    メール送信モデルメソッド化

--- app/models/ec_data/order_product.rb
+++ app/models/ec_data/order_product.rb
@@ -1,5 +1,15 @@
 module EcData
     class OrderProduct < EcData::Base
         belongs_to :stock, class_name:"Stock"
+
+        def send_shipment_complete
+            user = User.find_by(id:self.user_id)
+            order_list = EcData::OrderList.find_by(id:self.order_list_id)
+            buyer_address = order_list.buyer_address
+            sending_address = order_list.sending_address unless buyer_address.same_sending_address
+            product = EcData::Product.find_by(id:self.product_id)
+            postage = user.postages.find_by(item_type:product.class_name)
+            OrderMailer.shipment_notification(order:order,buyer_address:buyer_address,sending_address:sending_address ,product:product,postage:postage)
+        end
     end
 end
