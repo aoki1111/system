ccde0701ff6450028859c1e44ff3ac1765fed7b0
Y.Suzuki <ys3128@gmail.com>
2018-08-05 17:02:52 +0900

    order_productモデルメールメソッド修正

--- app/models/ec_data/order_product.rb
+++ app/models/ec_data/order_product.rb
@@ -8,7 +8,7 @@ module EcData
             sending_address = order_list.sending_address unless buyer_address.same_sending_address
             product = EcData::Product.find_by(id:self.product_id)
             postage = user.postages.find_by(item_type:product.class_name)
-            OrderMailer.shipment_notification(order:order,buyer_address:buyer_address,sending_address:sending_address ,product:product,postage:postage)
+            OrderMailer.shipment_notification(order:self,buyer_address:buyer_address,sending_address:sending_address ,product:product,postage:postage)
         end
     end
 end
